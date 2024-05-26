1a74253c29a10337834a6bb8bcb38b7441996e3d
Arif Nur Rizqi <arifnurrizqi@gmail.com>
2024-05-26 17:06:15 +0700

    add arduino file

---
+++ arduino_file_package_scales/arduino_file_package_scales.ino
@@ -0,0 +1,341 @@
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+// #include "soc/rtc.h"
+#include "HX711.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
+// define pinout used
+#define CALIBRATION_FACTOR 98
+#define LOADCELL_DT_PIN 18
+#define LOADCELL_SCK_PIN 19
+// #define RESET_BUTTON_PIN 2
+#define SPARE_BUTTON_PIN 4
+// Replace the next variables with your SSID/Password combination
+const char* ssid = "YOI";
+const char* password = "12345678";
+// Add your MQTT Broker IP address:
+const char* mqtt_server = "broker.hivemq.com";
+const char* mqtt_client_name = "atuo_weight_monitor";
+const char* mqtt_topic_weight = "smart-scale/weight";
+const char* mqtt_topic_volume = "smart-scale/volume";
+//define sound speed in cm/uS
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+//define lcd i2c dimension
+#define LCD_ROW 4
+#define LCD_COLOUMN 20
+// 0 atas// 1 kanan// 2 depan// 3 kiri// 4 belakang
+const int trigPin[] = {13,14,26,33,15};
+const int echoPin[] = {12,27,25,32,5};
+float distanceArr[5];
+float distanceLimit[] = {46,25.3,21.2,25.3,20.7};
+LiquidCrystal_I2C lcd(0x27, LCD_COLOUMN, LCD_ROW);
+HX711 scale;
+WiFiClient espClient;
+PubSubClient client(espClient);
+// variable initiation
+long duration;
+float distanceCm;
+float weight;
+float volume;
+int a;
+char buffer[20];
+float panjang;
+float lebar;
+float tinggi;
+long currentmillis;
+long previousmillis;
+long previousmillis2;
+long interval=1000;
+long interval2=5000;
+//pendefinisian fungsi
+float measureDistance(int i);
+float measureVolume();
+float measureWeight();
+void displayWelcome();
+void displayData(float volume, float weight);
+void displayReset();
+void setup_wifi();
+void callback(char* topic, byte* message, unsigned int length);
+void reconnect();
+
+void setup() {
+  Serial.begin(115200); // Starts the serial communication
+
+  for(int i=0;i<5;i++){
+    pinMode(trigPin[i], OUTPUT); // Sets the trigPin as an Output
+    pinMode(echoPin[i], INPUT); // Sets the echoPin as an Input
+  }
+  // pinMode(RESET_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(SPARE_BUTTON_PIN, INPUT_PULLUP);
+  // LCD setup
+  lcd.init(); // initialize LCD
+  lcd.backlight(); // turn on LCD backlight
+  displayWelcome();
+  // loadcell setup
+  // rtc_clk_cpu_freq_set(RTC_CPU_FREQ_80M);
+  scale.begin(LOADCELL_DT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(CALIBRATION_FACTOR);
+  scale.tare(); // reset the scale to 0
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  currentmillis = millis();
+  if(currentmillis - previousmillis >= interval){
+    previousmillis = currentmillis;
+    volume = measureVolume();
+    weight = measureWeight();
+    displayData(volume, weight);
+    Serial.println("Publishing data...");
+    sprintf(buffer, "%d", int(weight));
+    client.publish(mqtt_topic_weight, buffer);
+    sprintf(buffer, "%d", int(volume));
+    client.publish(mqtt_topic_volume, buffer);
+    Serial.println(digitalRead(SPARE_BUTTON_PIN));
+    if(digitalRead(SPARE_BUTTON_PIN) == LOW){
+      displayReset();
+      scale.tare();
+      while (digitalRead(SPARE_BUTTON_PIN) == LOW) {
+        delay(10);
+      }
+    }
+  }
+  if(Serial.available()){
+    switch(Serial.read()) // akhiri setiap perintah dengan ';' contoh "s1000;"
+    {
+      case 's':
+        a = Serial.parseInt();
+        for(int i=0;i<5;i++){
+          measureDistance(a);
+        } 
+        Serial.println();
+        break;
+
+      case 'a':
+        Serial.println("test");
+        break;
+
+      case 'b':
+        // set cursor to first column, first row
+        lcd.setCursor(0, 0);
+        // print message
+        lcd.print("Hello, World!");
+        delay(1000);
+        // clears the display to print new message
+        lcd.clear();
+        // set cursor to first column, second row
+        lcd.setCursor(0,1);
+        lcd.print("Hello, World!");
+        delay(1000);
+        lcd.clear();
+        break;
+
+      case 'c':
+        if (scale.is_ready()) {
+          scale.set_scale();
+          Serial.println("Tare... remove any weights from the scale.");
+          delay(5000);
+          scale.tare();
+          Serial.println("Tare done...");
+          Serial.print("Place a known weight on the scale...");
+          delay(5000);
+          long reading = scale.get_units(10);
+          Serial.print("Result: ");
+          Serial.println(reading);
+        }
+        else {
+          Serial.println("HX711 not found.");
+        }
+        break;
+
+      case 'd':
+        Serial.print("one reading:\t");
+        Serial.print(scale.get_units(), 1);
+        Serial.print("\t| average:\t");
+        Serial.println(scale.get_units(10), 5);
+        break;
+
+      case 'e':
+        scale.tare();
+        delay(500);
+        Serial.print("one reading:\t");
+        Serial.print(scale.get_units(), 1);
+        Serial.print("\t| average:\t");
+        Serial.println(scale.get_units(10), 5);
+        break;
+
+      case 'f':
+        measureVolume();
+        break;
+    }
+  }
+}
+
+float measureDistance(int i){
+  // Clears the trigPin
+  digitalWrite(trigPin[i], LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin[i], HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin[i], LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin[i], HIGH);
+
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED/2;
+  // Prints the distance in the Serial Monitor
+  Serial.print("Channel ");
+  Serial.print(i);
+  Serial.print(" Distance (cm): ");
+  Serial.println(distanceCm);
+  // delay(1000);
+  return distanceCm;
+}
+
+float measureWeight(){
+  return scale.get_units(10);
+}
+
+float measureVolume(){
+  //float panjang;
+  //float lebar;
+  //float tinggi;
+  float volume;
+  for(int i=0;i<5;i++){
+  distanceArr[i] = measureDistance(i);
+  }
+  panjang = ((distanceLimit[1] - distanceArr[1]) + (distanceLimit[3] -
+  distanceArr[3]));
+  lebar = ((distanceLimit[2] - distanceArr[2]) + (distanceLimit[4] -
+  distanceArr[4]));
+  tinggi = distanceLimit[0] - distanceArr[0];
+  volume = (panjang * lebar * tinggi);
+  Serial.print("panjang: ");
+  Serial.print(panjang);
+  Serial.print(" lebar: ");
+  Serial.print(lebar);
+  Serial.print(" tinggi: ");
+  Serial.print(tinggi);
+  Serial.print(" volume: ");
+  Serial.println(volume);
+  // if(distanceArr[0] < distanceLimit[0]){
+  // return 0;
+  // }else{
+  // return volume;
+  // }
+  return volume;
+}
+
+void displayData(float volume, float weight){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("P:");
+  lcd.print(panjang);
+  lcd.setCursor(7,0);
+  lcd.print("cm");
+  lcd.setCursor(10, 0);
+  lcd.print("L:");
+  lcd.print(lebar);
+  lcd.setCursor(18,0);
+  lcd.print("cm");
+  lcd.setCursor(5, 1);
+  lcd.print("T : ");
+  lcd.print(tinggi);
+  lcd.setCursor(13,1);
+  lcd.print("cm");
+  lcd.setCursor(0, 2);
+  lcd.print("Volume: ");
+  lcd.print(volume);
+  lcd.setCursor(16,2);
+  lcd.print("cm3");
+  lcd.setCursor(0, 3);
+  lcd.print("Berat: ");
+  lcd.print(weight);
+  lcd.setCursor(15,3);
+  lcd.print("gram");
+}
+
+void displayWelcome(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("ALAT PENGUKUR BERAT");
+  lcd.setCursor(8, 1);
+  lcd.print("dan");
+  lcd.setCursor(4, 2);
+  lcd.print("DIMENSI PAKET");
+  delay(3000);
+}
+
+void displayReset(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Kalibrasi");
+  delay(1000);
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+
+  for (int i = 0; i < length; i++) {
+  Serial.print((char)message[i]);
+  messageTemp += (char)message[i];
+  }
+    Serial.println();
+    // Feel free to add more if statements to control more GPIOs with MQTT
+    if (String(topic) == "EWS/esp32/output") {
+    Serial.print("Changing output to ");
+    if(messageTemp == "on"){
+      Serial.println("on");
+    }
+    else if(messageTemp == "off"){
+      Serial.println("off");
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+  // Serial.print("Attempting MQTT connection...");
+  // Attempt to connect
+  if (client.connect("EWS - Fire Alarm")) {
+    // Serial.println("connected");
+    // Subscribe
+    client.subscribe("EWS/esp32/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println("try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
