56978234ff92ab8f027e444266b621e8b040d613
Arif Nur Rizqi <69084186+arifnurrizqi@users.noreply.github.com>
2024-06-13 18:51:57 +0700

    Update script.js
    menambahkan format tanggal dd/mm/yyyy

--- arduino_file_package_scales/data/script.js
+++ arduino_file_package_scales/data/script.js
@@ -50,14 +50,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   function updateDateTime() {
     const now = new Date();
-    const date = now.toLocaleDateString();
+    
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const year = now.getFullYear();
     
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
 
+    const dateString = `${day}/${month}/${year}`;
     const time = `${hours}:${minutes}`;
     
-    document.getElementById('date').textContent = date;
+    document.getElementById('date').textContent = dateString;
     document.getElementById('time').textContent = time;
   }
 
